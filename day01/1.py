fc04fc2f2ee50e9d70cccc52822d82a924a62f4e
Dmytro Ivanov <dmytro@beardsvibe.com>
2018-12-01 12:01:04 +0100

    shorter

--- day01/1.py
+++ day01/1.py
@@ -1,2 +1,1 @@
-with open("input1.txt", "r") as f:
-	print(sum([int(x.strip()) for x in f.readlines()]))
+print(sum([int(x.strip()) for x in open("input1.txt").readlines()]))
--- day01/2.py
+++ day01/2.py
@@ -2,11 +2,10 @@ from itertools import cycle
 
 freq_last = 0
 freq_visited = set([freq_last])
-with open("input1.txt", "r") as f:
-	for delta in cycle([int(x.strip()) for x in f.readlines()]):
-		freq_last += delta
-		if freq_last in freq_visited:
-			print("seen " + str(freq_last) + " twice")
-			break
-		else:
-			freq_visited.add(freq_last)
+for delta in cycle([int(x.strip()) for x in open("input1.txt").readlines()]):
+	freq_last += delta
+	if freq_last in freq_visited:
+		print("seen " + str(freq_last) + " twice")
+		break
+	else:
+		freq_visited.add(freq_last)
