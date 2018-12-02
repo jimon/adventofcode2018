f50e92721fa4ef2d13cac9c3227406883959e18b
Dmytro Ivanov <dmytro@beardsvibe.com>
2018-12-02 11:25:26 +0100

    refactored

--- day02/1.py
+++ day02/1.py
@@ -1,6 +1,8 @@
 from collections import Counter
 
-# list of lists which contain count of single character occurences in a string
-data = [[v for k, v in Counter(list(x.strip())).items()] for x in open("input1.txt").readlines()]
+# "for x in open("input1.txt").readlines()" makes a generator that reads all lines one by one
+# "Counter(x.strip()).values()" counts occurences of every symbol in a line and return array of how many time every symbol occured
+# "set( )" only leaves unique occurences, so if two symbols occured twice we only leave one 2
+data = Counter(v for x in open("input1.txt").readlines() for v in set(Counter(x.strip()).values()))
 
-print(sum([1 for x in data if 2 in x]) * sum([1 for x in data if 3 in x]))
+print(data[2] * data[3])
