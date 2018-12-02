49e973f27562cfa0dc401b21bec5e558a16c298f
Dmytro Ivanov <dmytro@beardsvibe.com>
2018-12-02 11:53:44 +0100

    one liner

--- day02/2.py
+++ day02/2.py
@@ -1,10 +1,9 @@
-from itertools import compress, combinations
+from itertools import combinations
 
-def dist(x, y):
-	assert(len(x) == len(y))
-	return sum([1 for i in range(0, len(x)) if x[i] != y[i]])
-
-input_strings = [list(x.strip()) for x in open("input1.txt").readlines()]
-data = next(''.join([x[i] for i in range(0, len(x)) if x[i] == y[i]]) for x, y in combinations(input_strings, 2) if dist(x, y) == 1)
+# [x.strip() for x in open("input1.txt").readlines()] makes list of all input strings
+# for x, y in combinations(..., 2) permutates all possible pair strings as AB AC ...
+# if len([1 for a, b in zip(x, y) if a != b]) == 1 checks if distance between two strings is exactly one character
+# [a for a, b in zip(x, y) if a == b] finds common characters between two strings
+data = next(''.join([a for a, b in zip(x, y) if a == b]) for x, y in combinations([x.strip() for x in open("input1.txt").readlines()], 2) if len([1 for a, b in zip(x, y) if a != b]) == 1)
 
 print(data)
