0b3af09e4b937d3400aff490470f12829d3a31dc
Dmytro Ivanov <dmytro@unity3d.com>
2018-12-11 11:40:14 +0100

    task2

--- day11/2.py
+++ day11/2.py
@@ -1,41 +1,51 @@
 import numpy as np
 
-grid_serial_number = 18
+grid_serial_number = 1718
 
-def getpower(x, y, grid_serial_number):
+def getpower(x, y):
     rack_id = x + 10
     power = int((((rack_id * y + grid_serial_number) * rack_id) % 1000) / 100) - 5
     return power
 
-grid = np.array([ getpower(x, y, grid_serial_number) for y in range(1, 301) for x in range(1, 301) ])
+grid_width = 300
+
+grid = [ getpower(x, y) for y in range(1, grid_width + 1) for x in range(1, grid_width + 1) ]
+sum_grid = [ 0 for y in range(1, grid_width + 1) for x in range(1, grid_width + 1) ]
+
+def toi(x, y):
+    return (y - 1) * grid_width + (x - 1)
+
+def sum_or_zero(x, y):
+    if x < 1 or y < 1 or x > grid_width or y > grid_width:
+        return 0
+    else:
+        return sum_grid[toi(x, y)]
+
+for y in range(1, grid_width + 1):
+    for x in range(1, grid_width + 1):
+        sum_grid[toi(x, y)] = sum_or_zero(x - 1, y) + sum_or_zero(x, y - 1) - sum_or_zero(x - 1, y - 1) + grid[toi(x, y)]
+
+
+# for y in range(1, grid_width + 1):
+#     print(', '.join(['%06u' % grid[toi(x, y)] for x in range(1, grid_width + 1)]))
+# print('-----')
+# for y in range(1, grid_width + 1):
+#     print(', '.join(['%06u' % sum_grid[toi(x, y)] for x in range(1, grid_width + 1)]))
 
 max_power = None
 max_power_x = None
 max_power_y = None
-max_power_w = None
+max_power_y = None
 
-for y0 in range(1, 10):
-    print((y0 * 300 + 0) / (300 * 300 + 300))
-    for x0 in range(1, 10):
-        for w in range(0, 20):#300 - max(x0, y0) + 1):
-            if x0 + w > 300 or y0 + w > 300:
-                continue
-            assert(x0 + w <= 300)
-            assert(y0 + w <= 300)
-
-            power = np.sum([np.sum(grid[y*300+x0:x0+w+1]) for y in range(y0, y0 + w + 1)])
-            print(power)
-
-            if max_power is None or power > max_power:
-                max_power = power
-                max_power_x = x0
-                max_power_y = y0
+for y in range(1, grid_width + 1):
+    print(y / (grid_width + 1))
+    for x in range(1, grid_width + 1):
+        for w in range(1, min(grid_width - max(x, y) + 1, 18)):
+            sum_power = sum_grid[toi(x + w - 1, y + w - 1)] - sum_or_zero(x - 1, y + w - 1) - sum_or_zero(x + w - 1, y - 1) + sum_or_zero(x - 1, y - 1)
+            if max_power is None or sum_power > max_power:
+                max_power = sum_power
+                max_power_x = x
+                max_power_y = y
                 max_power_w = w
 
 print(max_power_x, max_power_y, max_power_w, max_power)
-
-
-#square_powers = [(x0, y0, w, sum([power(x0 + x, y0 + y, 1718) for x in range(0, 3) for y in range(0, 3)])) for x0 in range(1, 298) for y0 in range(1, 298)]
-#square_powers = sorted(square_powers, key = lambda x: x[2], reverse=True)
-
-#print(square_powers[0])
