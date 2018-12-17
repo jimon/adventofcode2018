1a00095fa50176bb3288eee2b0fd19aedfe54b0c
Dmytro Ivanov <dmytro@beardsvibe.com>
2018-12-17 20:49:01 +0100

    day17 task2

--- day17/1.py
+++ day17/1.py
@@ -51,37 +51,35 @@ def sim(m):
             if get(m, x, y) in ['+', '|']:
                 if get(m, x, y + 1) == '.':
                     set(m, x, y + 1, '|')
-                elif get(m, x, y + 1) == '#':
-                    set(m, x, y, '~')
-                elif get(m, x, y + 1) == '~' and is_row_bound(m, x, y + 1):
-                    set(m, x, y, '~')
-            elif get(m, x, y) == '~':
-                if get(m, x - 1, y) == '.':
-                    if get(m, x - 1, y + 1) == '.':
-                        set(m, x - 1, y, '|')
+                elif get(m, x, y + 1) in ['#', '~']:
+                    xl = x - 1
+                    xr = x + 1
+                    while get(m, xl, y + 1) in ['#', '~'] and get(m, xl, y) in ['.', '|']:
+                        xl -= 1
+                    while get(m, xr, y + 1) in ['#', '~'] and get(m, xr, y) in ['.', '|']:
+                        xr += 1
+                    if get(m, xl, y) == '#' and get(m, xr, y) == '#':
+                        for x2 in range(xl + 1, xr):
+                            set(m, x2, y, '~')
                     else:
-                        set(m, x - 1, y, '~')
-                elif get(m, x + 1, y) == '.':
-                    if get(m, x + 1, y + 1) == '.':
-                        set(m, x + 1, y, '|')
-                    else:
-                        set(m, x + 1, y, '~')
+                        for x2 in range(xl + 1 - (1 if get(m, xl, y) != '#' else 0), xr + (1 if get(m, xr, y) != '#' else 0)):
+                            set(m, x2, y, '|')
 
-    pass
-
-def deb(m):
+def deb(m, max_h = None):
     print('-----')
-    for y in range(0, h):
+    for y in range(0, max_h if max_h is not None else h):
         print(''.join(m[y]))
 
 #deb(m)
-for i in range(0, 9000):
+for i in range(0, 1000):
     if i % 100 == 0:
         print(i)
     sim(m)
 deb(m)
 
 total = len([1 for y in range(0, h) for x in range(0, w) if y >= y0org and get(m, x, y) in ['|', '~']])
+total2 = len([1 for y in range(0, h) for x in range(0, w) if y >= y0org and get(m, x, y) == '~'])
 print(total)
+print(total2)
 
 # 29068 is too high
